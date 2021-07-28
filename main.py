11b53621a68767fae50c198ca4732cd60c4458c9
desmondlb <69417904+desmondlb@users.noreply.github.com>
2021-07-28 23:39:07 +0530

    :seedling: Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import random
+from tqdm import tqdm
+import numpy as np
+import torch, torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data.dataset import Dataset
+torch.backends.cudnn.benchmark=True
