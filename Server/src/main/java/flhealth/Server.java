d11289e618b29dc0737707b16627f707ff0dc3cb
Desmond <desmondlb@gmail.com>
2021-10-26 23:10:53 +0530

    added server

---
+++ Server/src/main/java/flhealth/Server.java
@@ -0,0 +1,13 @@
+package flhealth;
+
+
+public class Server {
+
+    public static void main(String[] args) throws  Exception {
+
+       Model model = new Model();
+       model.TrainModel();
+
+    }
+
+}
